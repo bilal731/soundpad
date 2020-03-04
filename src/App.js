82bfa0c2d82f64c82139b081edf1b1feb3a7c06e
Bilal <bilalsadik23@gmail.com>
2020-03-04 22:17:29 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Layout } from "antd";
+import "./sound.css";
+import Sound from "./Sound";
+import audio1 from "./tracks/1.wav";
+import audio2 from "./tracks/2.wav";
+import audio3 from "./tracks/3.wav";
+import audio4 from "./tracks/4.wav";
+import audio5 from "./tracks/5.wav";
+import audio6 from "./tracks/6.wav";
+import audio7 from "./tracks/7.mp3";
+import audio8 from "./tracks/8.wav";
+import audio9 from "./tracks/9.wav";
+import audio10 from "./tracks/10.wav";
+import audio11 from "./tracks/11.mp3";
+import audio12 from "./tracks/12.wav";
+import audio13 from "./tracks/13.wav";
+import audio14 from "./tracks/14.wav";
+import audio15 from "./tracks/15.aiff";
+import audio16 from "./tracks/16.wav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Layout>
+        
+          <div className="main">
+          <div>
+              <Sound audio={audio1} />
+        
+            </div>
+            <div>
+              <Sound audio={audio2} />
+            </div>
+            <div>
+              <Sound audio={audio3} />
+            </div>
+            <div>
+              <Sound audio={audio4} />
+            </div>
+            <div>
+              <Sound audio={audio5} />
+            </div>
+            <div>
+              <Sound audio={audio6} />
+            </div>
+            <div>
+              <Sound audio={audio7} />
+            </div>
+            <div>
+              <Sound audio={audio8} />
+            </div>
+            <div>
+              <Sound audio={audio9} />
+            </div>
+            <div>
+              <Sound audio={audio10} />
+            </div>
+            <div>
+              <Sound audio={audio11} />
+            </div>
+            <div>
+              <Sound audio={audio12} />
+            </div>
+            <div>
+              <Sound audio={audio13} />
+            </div>
+            <div>
+              <Sound audio={audio14} />
+            </div>
+            <div>
+              <Sound audio={audio15} />
+            </div>
+            <div>
+              <Sound audio={audio16} />
+            </div>
+          </div>
+        
+      </Layout>
     </div>
   );
 }
